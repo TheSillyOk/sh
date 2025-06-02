673ed1fc1b2c3b9c1ca43b4a742088fb67992818
TheSillyOk <priv.ld@proton.me>
2025-06-02 17:30:05 -0300

    Update upload-assets.sh to use non escaped variable when uploading
    Signed-off-by: TheSillyOk <priv.ld@proton.me>

--- upload-assets.sh
+++ upload-assets.sh
@@ -4,7 +4,7 @@ for kernel in *; do
     if [ -d "$kernel" ]; then
        escaped_kernel=$(printf '%q' "$kernel")
        echo "Zipping $escaped_kernel..."
-       zip -r "$escaped_kernel.zip" $kernel/
+       zip -r "$kernel.zip" $kernel/
        rm -rf $kernel/
        echo "Uploading $escaped_kernel.zip..."
        gh release upload "$1" "$escaped_kernel.zip"
