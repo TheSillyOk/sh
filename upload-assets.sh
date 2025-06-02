51c53be95b34150e9db92eeab9fc122aab3d5a70
TheSillyOk <priv.ld@proton.me>
2025-06-02 17:37:43 -0300

    Update upload-assets.sh to upload kernels correctly
    Signed-off-by: TheSillyOk <priv.ld@proton.me>

--- upload-assets.sh
+++ upload-assets.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
-for kernel in *; do
+cd ..
+git clone https://github.com/TheSillyOk/kernel_laurel_sprout_workflows --depth=1 --branch "$1" repo
+cd repo
+for kernel in ../downloaded-artifacts/*; do
     if [ -d "$kernel" ]; then
        escaped_kernel=$(printf '%q' "$kernel")
        echo "Zipping $escaped_kernel..."
-       zip -r "$kernel.zip" $kernel/
-       rm -rf $kernel/
+       zip -r "../downloaded-artifacts/$kernel.zip" "../downloaded-artifacts/$kernel/"
+       rm -rf "../downloaded-artifacts/$kernel/"
        echo "Uploading $escaped_kernel.zip..."
-       gh release upload "$1" "$escaped_kernel.zip"
+       gh release upload "$1" "../downloaded-artifacts/$escaped_kernel.zip"
     fi
 done
