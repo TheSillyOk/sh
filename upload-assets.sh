2ee8d0ccb56f8f9561ec5e231f038b3cd55f00e9
TheSillyOk <priv.ld@proton.me>
2025-06-04 06:42:53 -0300

    upload-assets.sh: make paths more precise
    Signed-off-by: TheSillyOk <priv.ld@proton.me>

--- upload-assets.sh
+++ upload-assets.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-for kernel in ../downloaded-artifacts/*; do
+tag="$1"
+repo_dir="$2"
+artifacts_dir="$3"
+
+cd $repo_dir
+
+for kernel in $artifacts_dir/*; do
     if [ -d "$kernel" ]; then
        escaped_kernel=$(printf '%q' "$kernel")
        echo "Zipping $escaped_kernel..."
        zip -r "$kernel.zip" "$kernel/"
        rm -rf "$kernel/"
        echo "Uploading $escaped_kernel.zip..."
-       gh release upload "$1" "$escaped_kernel.zip"
+       gh release upload "$tag" "$escaped_kernel.zip"
     fi
 done
