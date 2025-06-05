0189ebee8beec3951847f1ba447dd108b373ee82
TheSillyOk <priv.ld@proton.me>
2025-06-05 08:41:36 -0300

    upload-assets.sh: attempt to fix zip files
    Signed-off-by: TheSillyOk <priv.ld@proton.me>

--- upload-assets.sh
+++ upload-assets.sh
@@ -4,15 +4,16 @@ tag="$1"
 repo_dir="$2"
 artifacts_dir="$3"
 
-cd $repo_dir
-
-for kernel in $artifacts_dir/*; do
+cd $artifacts_dir
+for kernel in *; do
     if [ -d "$kernel" ]; then
        escaped_kernel=$(printf '%q' "$kernel")
        echo "Zipping $escaped_kernel..."
        zip -r "$kernel.zip" "$kernel/"
        rm -rf "$kernel/"
        echo "Uploading $escaped_kernel.zip..."
-       gh release upload "$tag" "$escaped_kernel.zip"
+       cd $repo_dir
+       gh release upload "$tag" "$artifacts_dir/$escaped_kernel.zip"
+       cd $artifacts_dir
     fi
 done
